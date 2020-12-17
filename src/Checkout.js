7334de5bc117b03055a96d1db1e695266b7b464b
Omkar Acharya <49238582+Acharya-Omkar@users.noreply.github.com>
2020-12-17 14:20:20 +0530

    login page commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,29 +5,33 @@ import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
 
 function Checkout() {
-  const [{basket} , dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
-  <div className="checkout">
+    <div className="checkout">
       <div className="checkout__left">
-        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/CEPC/WFH/ProHomeOffice/wearables_banner_1500x200.jpg" alt="Amazon ad" className="checkout__ad" />
+        <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/CEPC/WFH/ProHomeOffice/wearables_banner_1500x200.jpg"
+          alt="Amazon ad"
+          className="checkout__ad"
+        />
         <div>
-            <h2 className="checkout__title">Your shopping basket</h2>
-              {basket.map(item => (
-              <CheckoutProduct 
-              id = {item.id}
-              title= {item.title}
-              image= {item.image}
-              price= {item.price}
-              rating= {item.rating}
-
-              />
-              ))}
+          <h3>Hello, {user?.email}</h3>
+          <h2 className="checkout__title">Your shopping basket</h2>
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout__right">
-          <Subtotal />
+        <Subtotal />
       </div>
-  </div>
+    </div>
   );
 }
 
