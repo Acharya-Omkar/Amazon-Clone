9fcc65248b3873ac687b56dc6ff5f8f0bed08aa7
Omkar Acharya <49238582+Acharya-Omkar@users.noreply.github.com>
2020-12-20 18:45:35 +0530

    tweaks

--- src/Header.js
+++ src/Header.js
@@ -20,7 +20,7 @@ function Header() {
       <Link to="/">
         <img
           className="header__logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
       </Link>
       <div className="header__search">
@@ -31,18 +31,22 @@ function Header() {
       <div className="header__nav">
         <Link to={!user && "/login"}>
           <div onClick={handleAuthentication} className="header__option">
-  <span className="header__optionLineOne">Hello, {!user ? 'Guest' : user.email}</span>
+            <span className="header__optionLineOne">
+              Hello, {!user ? "Guest" : user.email}
+            </span>
             <span className="header__optionLineTwo">
               {" "}
               {user ? "Sign Out" : "Sign In"}
             </span>
           </div>
         </Link>
-        <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo">Orders</span>
-        </div>
 
+        <Link to="/orders">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">Orders</span>
+          </div>
+        </Link>
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
